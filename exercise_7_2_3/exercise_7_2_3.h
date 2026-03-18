dba6e571611ad953926b2439ec54a613a42823a4
Ryan <ryanvermeulen08@outlook.com>
2026-03-18 16:54:07 +0100

    add header file for exercise_7_2_3

---
+++ exercise_7_2_3/exercise_7_2_3.h
@@ -0,0 +1,107 @@
+#ifndef EXERCISE_7_2_3_H
+#define EXERCISE_7_2_3_H
+
+#include <string>
+
+class Article
+{
+    public:
+        // constructors
+        Article();
+        Article(const std::string& name,const int& price);
+
+        // copy
+        Article(const Article& other);
+
+        // move
+        Article(Article&& other) noexcept;
+
+        // getters
+        std::string get_name() const;
+        int get_price() const;
+        void print_info() const;
+
+        // setters
+        void set_name(const std::string& name);
+        void set_price(const int& price);
+    private:
+        std::string m_name{};
+        int m_price{};
+};
+
+class Game : public Article
+{
+public:
+    // contstructor to initialise base class (Article)
+    Game(const std::string& name, int price);
+    // default constructor
+    Game();
+    // converting constructor
+    Game(const std::string& type, int esrb);
+    // destructor
+    ~Game();
+
+    // getters
+    std::string get_type() const;
+    int get_esrb() const;
+    void print_info() const;
+
+    // setters
+    void set_type(const std::string& type);
+    void set_esrb(int esrb);
+private:
+    std::string m_type{}; // e.g. pc, ps4, xbox one, etc.
+    int m_esrb{};
+};
+
+class Book : public Article
+{
+public:
+    // constructor to initialise base class (Article)
+    Book(const std::string& name, int price);
+    // default constructor
+    Book();
+    // converting constructor
+    Book(const std::string& author, const std::string& isbn);
+    // destructor
+    ~Book();
+
+    // getters
+    std::string get_author() const;
+    std::string get_isbn() const;
+    void print_info() const;
+
+    // setters
+    void set_author(const std::string& author);
+    void set_isbn(const std::string& isbn);
+private:
+    std::string m_author{};
+    std::string m_isbn{};
+};
+
+class Comic : public Book
+{
+public:
+    // constructor to initialise base class (Article)
+    Comic(const std::string& name, int price);
+    // default constructor
+    Comic();
+    // converting constructor
+    Comic(const std::string& serial_name, int serial_number);
+    // destructor
+    ~Comic();
+
+    // getters
+    std::string get_serial_name() const;
+    int get_serial_number() const;
+    void print_info() const;
+
+    // setters
+    void set_serial_name(const std::string& serial_name);
+    void set_serial_number(const int& serial_number);
+
+private:
+    std::string m_serial_name{};
+    int m_serial_number{};
+};
+#endif
